e473dae14602f99d3c22ebe1928d041bf3438f3e
lbovet <laurent.bovet@windmaster.ch>
2014-10-28 11:46:55 +0100

    Refined right box

--- assets/js/github.js
+++ assets/js/github.js
@@ -3,28 +3,33 @@ jQuery.githubUser = function(username, callback) {
 }
 
 jQuery.fn.loadRepositories = function(username) {
+    var ts = new Date().getTime(); // cache expire
     var html = "<img src='/assets/themes/swisspush/images/loading.gif'>";
     if(sessionStorage) {
+        ts = sessionStorage.getItem("swisspush.repos.ts") || ts;
         html = sessionStorage.getItem("swisspush.repos") || html;
     }
     this.html(html);
 
     var target = this;
-    $.githubUser(username, function(data) {
-        var repos = data.data; // JSON Parsing
-        sortByName(repos);
+    if(ts <= new Date().getTime()) {
+        $.githubUser(username, function (data) {
+            var repos = data.data; // JSON Parsing
+            sortByName(repos);
 
-        var list = $('<ul class="nav nav-stacked"/>');
-        target.empty().append(list);
-        $(repos).each(function() {
-            if (this.name != (username.toLowerCase()+'.github.io')) {
-                list.append('<li><a title="'+this.description+'" href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></li>');
+            var list = $('<ul class="horizontal"/>');
+            target.empty().append(list);
+            $(repos).each(function () {
+                if (this.name != (username.toLowerCase() + '.github.io')) {
+                    list.append('<li><a title="' + this.description + '" href="' + (this.homepage ? this.homepage : this.html_url) + '">' + this.name + '</a></li>');
+                }
+            });
+            if (sessionStorage) {
+                sessionStorage.setItem("swisspush.repos", target.html());
+                sessionStorage.setItem("swisspush.repos.ts", ts+1*3600*1000);
             }
         });
-        if(sessionStorage) {
-            sessionStorage.setItem("swisspush.repos", target.html());
-        }
-    });
+    }
 
     function sortByName(repos) {
         repos.sort(function(a,b) {
